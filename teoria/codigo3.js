4767fa34376120be48efbba097191f3c11799fe1
mgeg2 <158155590+mgeg2@users.noreply.github.com>
2024-03-28 13:23:22 +0100

    Add files via upload

--- teoria/codigo3.js
+++ teoria/codigo3.js
@@ -3,43 +3,43 @@ function receta() {
     const ID = urlParams.get('ID');
 
     let url = 'api/recetas/' + ID;
-	let xhr = new XMLHttpRequest();
-    
-    xhr.open('GET',url,true);
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url, true);
     xhr.responseType = 'json';
- 
-    xhr.onload = function() {
+
+    xhr.onload = function () {
         let r = xhr.response;
-        console.log(r);
-        if (r.RESULTADO =='OK') { 
-            let html='';
-           r.FILAS.forEach(function(receta,idx){
-            let estrellas;
+        // console.log(r);
+        if (r.RESULTADO == 'OK') {
+            let html = '';
+            r.FILAS.forEach(function (receta, idx) {
+                let estrellas;
 
-				switch ( parseInt(receta.dificultad)) {
-					case 0:
-						estrellas = `<span class="material-symbols-rounded"><i>star</i></span>`;
-						break
-					case 1:
-						estrellas = `<span class="material-symbols-rounded"><i>star</i><i>star</i></span>`;
-						break;
-					case 2:
-						estrellas = `<span class="material-symbols-rounded"><i>star</i><i>star</i><i>star</i></span>`;
-						break;
-					default:
-						estrellas = `<p>Opción por defecto</p>`;
-				}
-            var fechaOriginal =receta.fechaCreacion ;
+                switch (parseInt(receta.dificultad)) {
+                    case 0:
+                        estrellas = `<p>Baja</p>`;
+                        break
+                    case 1:
+                        estrellas =`<p>Media</p>`;
+                        break;
+                    case 2:
+                        estrellas = `<p>Alta</p>`;
+                        break;
+                    default:
+                        estrellas = `<p>Opción por defecto</p>`;
+                }
+                var fechaOriginal = receta.fechaCreacion;
 
-            // Dividir la fecha en día, mes y año
-            var partesFecha = fechaOriginal.split('-');
-            var dia = partesFecha[2];
-            var mes = partesFecha[1];
-            var año = partesFecha[0];
-            var fechaGirada = dia + '/' + mes + '/' + año;
-            console.log(fechaGirada); // Salida: "03/20/2024"
-            console.log(receta);
-            html+=`
+                // Dividir la fecha en día, mes y año
+                var partesFecha = fechaOriginal.split('-');
+                var dia = partesFecha[2];
+                var mes = partesFecha[1];
+                var año = partesFecha[0];
+                var fechaGirada = dia + '/' + mes + '/' + año;
+                console.log(fechaGirada); // Salida: "03/20/2024"
+
+                html += `
             <div class="tituloyfecha">
                 <h1>${receta.nombre}</h1>
                 <p>${fechaGirada}</p>
@@ -56,44 +56,50 @@ function receta() {
                     <div id="etiqs"></div> <!-- Llamando a la función etiquetas aquí -->
                     
                 </div>
-            </div>`
-           });
-            document.querySelector('#receta-container').innerHTML=html;
-            
+            </div>
+            <h2>Ingredientes</h2>
+            <ul id="ing"></ul>
+            <h2>Pasos</h2>
+            <ol id="pasos"></ol>
+            `
+            });
+            document.querySelector('#receta-container').innerHTML = html;
+
             ingredientes();
             etiquetas();
+            pasos();
 
         }
     }
-        xhr.send();
-       
-       
+    xhr.send();
+
+
 
     // Usar el nombre de la receta según sea necesario
-   // Esto imprimirá el nombre de la receta en la consola
+    // Esto imprimirá el nombre de la receta en la consola
     // Aquí puedes agregar lógica adicional para cargar la receta según el nombre, etc.
- 
-   
+
+
 }
 
 function ingredientes() {
     const urlParams = new URLSearchParams(window.location.search);
     const ID = urlParams.get('ID');
 
-    let url = 'api/recetas/' + ID +'/ingredientes';
-	let xhr = new XMLHttpRequest();
-    
-    xhr.open('GET',url,true);
+    let url = 'api/recetas/' + ID + '/ingredientes';
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url, true);
     xhr.responseType = 'json';
     xhr.onload = function () {
         let r = xhr.response;
-        console.log(r);
-        if(r.RESULTADO== 'OK'){
-            let html ='';
-            r.FILAS.forEach(function(ingrediente,idx){
-                html +=`<li>${ingrediente.texto}</li>`;
+        // console.log(r);
+        if (r.RESULTADO == 'OK') {
+            let html = '';
+            r.FILAS.forEach(function (ingrediente, idx) {
+                html += `<li><p>${ingrediente.texto}</p></li>`;
             });
-            document.querySelector('#ing').innerHTML+=html;
+            document.querySelector('#ing').innerHTML += html;
         }
     }
     xhr.send();
@@ -103,24 +109,52 @@ function etiquetas() {
     const urlParams = new URLSearchParams(window.location.search);
     const ID = urlParams.get('ID');
 
-    let url = 'api/recetas/' + ID +'/etiquetas';
-	let xhr = new XMLHttpRequest();
-    
-    xhr.open('GET',url,true);
+    let url = 'api/recetas/' + ID + '/etiquetas';
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url, true);
     xhr.responseType = 'json';
     xhr.onload = function () {
         let r = xhr.response;
-        console.log(r);
-        if(r.RESULTADO== 'OK'){
-            let html ='';
-            r.FILAS.forEach(function(etiqueta,idx){
-                html +=`<p><a href=index.html>${etiqueta.nombre}</a></p>`;
-                
+        // console.log(r);
+        if (r.RESULTADO == 'OK') {
+            let html = '';
+            r.FILAS.forEach(function (etiqueta, idx) {
+                html += `<p><a href=index.html>${etiqueta.nombre}</a></p>`;
+
             });
 
-            document.querySelector('#etiqs').innerHTML+=html;
+            document.querySelector('#etiqs').innerHTML += html;
         }
     }
     xhr.send();
 
+}
+
+function pasos() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const ID = urlParams.get('ID');
+
+    let url = 'api/recetas/' + ID;
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+
+    xhr.onload = function () {
+        let r = xhr.response;
+         console.log(r);
+        if (r.RESULTADO == 'OK') {
+            let html = '';
+
+                 var pasos = r.FILAS[0].elaboracion.split("\n");
+    
+                console.log(pasos);
+                for (let i = 0; i < pasos.length; i++) {
+                    html +=`<li>${pasos[i]}</li>`;    
+                }
+            document.querySelector('#pasos').innerHTML += html;
+        }
+    }
+    xhr.send();
 }
